7ebe3a5e851166fb30e0d906fc825e00cada179a
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-11-24 17:48:28 +0100

    Modify model structure

--- Web_Computer_Service/account/models.py
+++ Web_Computer_Service/account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phone_field import PhoneField
+from django.utils import timezone
 # Create your models here.
 
 class User (AbstractUser): 
@@ -9,8 +10,10 @@ class User (AbstractUser):
 
 class Employee (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    phone_number = PhoneField(blank=True)
+    salary = models.SmallIntegerField(blank=True, null=True)
+    department = models.CharField(blank=False, max_length=20, default="Brak danych")
 
 class Customer (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    phone_number = PhoneField(blank=False)
+    birth_date = models.DateField(null=True)
+    account_create_date = models.DateTimeField(default=timezone.now)
