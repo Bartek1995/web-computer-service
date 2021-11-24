2df2e0140fe34aed6c92ce000ab84c446e659e42
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-11-24 18:08:49 +0100

    Register user model to admin page

--- Web_Computer_Service/account/admin.py
+++ Web_Computer_Service/account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+
+@admin.register(User)
+class AbstracUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "is_employee", "is_customer")
+    # fields = ("username", "first_name", "last_name", "email", "password")
