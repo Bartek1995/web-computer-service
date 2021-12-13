0b653790efe44a4db24f59b217c33d3ae96e3b5a
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-12-13 19:32:08 +0100

    Update urls.py - add main service page to url list

--- Web_Computer_Service/account/urls.py
+++ Web_Computer_Service/account/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import MainPage, ServiceMainPage
 
 app_name = "Web_Computer_Service"
 
 urlpatterns = [
-    path('', views.main, name="main")
+    path('', MainPage.as_view(), name="main"),
+    path('service', ServiceMainPage.as_view(), name="service")
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
