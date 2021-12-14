55440465e9f899c28ece02857a8c262344749149
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-12-14 12:34:02 +0100

    Add creaate_employee url to url.py

--- Web_Computer_Service/account/urls.py
+++ Web_Computer_Service/account/urls.py
@@ -3,11 +3,10 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import MainPage, ServiceMainPage
-
 app_name = "Web_Computer_Service"
 
 urlpatterns = [
     path('', MainPage.as_view(), name="main"),
-    path('service', ServiceMainPage.as_view(), name="service")
-
+    path('service', ServiceMainPage.as_view(), name="service"),
+    path('service/create_employee', views.create_employee, name='create_employee'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
