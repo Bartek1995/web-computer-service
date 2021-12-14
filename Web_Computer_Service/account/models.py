c08915d8e0dcdd226dc2ad6e84a125ca86096dff
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-12-14 12:11:28 +0100

    Modify model structure

--- Web_Computer_Service/account/models.py
+++ Web_Computer_Service/account/models.py
@@ -7,13 +7,6 @@ from django.utils import timezone
 class User (AbstractUser): 
     is_employee = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
-
-class Employee (models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    salary = models.SmallIntegerField(blank=True, null=True)
-    department = models.CharField(blank=False, max_length=20, default="Brak danych")
-
-class Customer (models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    birth_date = models.DateField(null=True)
-    account_create_date = models.DateTimeField(default=timezone.now)
+    address = models.CharField(max_length=50, default="Brak")
+    employee_account_number = models.IntegerField(null=True)
+    # account_create_date = models.DateTimeField(default=timezone.now)
