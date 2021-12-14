45efc9f04fa60d9ed0e28e9ab36583d24ae23f4f
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-12-14 12:11:15 +0100

    Create add employee feauture, set up view, form

---
+++ Web_Computer_Service/account/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
+
+
+class EmployeeCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+    label='Hasło',
+    strip=False,
+    widget=forms.PasswordInput(),
+    )
+
+    password2 = forms.CharField(
+    label='Potwierdź hasło',
+    strip=False,
+    widget=forms.PasswordInput(),
+    )
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'employee_account_number','is_employee']
+
+        labels = {
+            'username': "Nazwa użytkownika",
+            'employee_account_number': "Numer pracownika",
+        }
+
+    
--- Web_Computer_Service/account/views.py
+++ Web_Computer_Service/account/views.py
@@ -1,13 +1,28 @@
+from django.forms.forms import Form
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView, FormView
 from django.views.generic.base import TemplateView
+from .forms import EmployeeCreationForm
 
 
-# def main(request):
-#     return render(request, 'index.html')
+def main(request):
+    return render(request, 'index.html')
 
 class MainPage(LoginView):
     template_name = "account/index.html"
 
 class ServiceMainPage(TemplateView):
     template_name = 'service.html'
+
+def create_employee(request):
+    if request.method == "POST":
+        form = EmployeeCreationForm(request.POST)
+        if form.is_valid():
+            new_employee = form.save(commit=False)
+            new_employee.is_employee = 1
+            return render(request, 'service_functions/employee_create_complete.html', {'user': new_employee})
+
+    else:
+        form = EmployeeCreationForm()
+    return render(request, 'service_functions/add_employee.html', {'form': form})
