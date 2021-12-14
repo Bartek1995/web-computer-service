4827c8456b5532e8b135e943c1a7e8734ac7001f
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-12-14 12:32:28 +0100

    Modify admin.py

--- Web_Computer_Service/account/admin.py
+++ Web_Computer_Service/account/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 # Register your models here.
 
 
-@admin.register(User)
-class AbstracUserAdmin(admin.ModelAdmin):
-    list_display = ("username", "first_name", "last_name", "email", "is_employee", "is_customer")
-    # fields = ("username", "first_name", "last_name", "email", "password")
+# @admin.register(CustomUser)
+# class AbstracUserAdmin(admin.ModelAdmin):
+#     list_display = ("username", "first_name", "last_name", "email", "is_employee", "is_customer")
+#     # fields = ("username", "first_name", "last_name", "email", "password")
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(User, CustomUserAdmin)
