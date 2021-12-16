82d1db0471edd922f9d94699f1e80e3df41e1f2f
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-12-16 18:11:02 +0100

    Modify user model

--- Web_Computer_Service/account/models.py
+++ Web_Computer_Service/account/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phone_field import PhoneField
 from django.utils import timezone
+from phone_field import PhoneField
+
 # Create your models here.
 
 class User (AbstractUser): 
     is_employee = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
-    address = models.CharField(max_length=50, default="Brak")
+    address = models.CharField(max_length=50, default="Brak informacji")
+    phone = PhoneField(blank=True, help_text="Telefon kontaktowy")
     employee_account_number = models.IntegerField(null=True)
+    customer_account_number = models.IntegerField(null=True)
     # account_create_date = models.DateTimeField(default=timezone.now)
