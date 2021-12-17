33943d1b720e20b600deaa510a4948a6c0039139
Bartłomiej Swatek <bartek.swatek@wp.pl>
2021-12-17 17:25:59 +0100

    Add edit employee feature

--- Web_Computer_Service/account/urls.py
+++ Web_Computer_Service/account/urls.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
+from .views import LoginPage
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import MainPage, ServiceMainPage
+from .views import ServiceMainPage
 app_name = "Web_Computer_Service"
 
 urlpatterns = [
-    path('', MainPage.as_view(), name="main"),
+    path('', LoginPage.as_view(), name="login"),
+    path('', LogoutView.as_view(), name="logout"),
     path('service', ServiceMainPage.as_view(), name="service"),
     path('service/create_employee', views.create_employee, name='create_employee'),
     path('service/create_customer', views.create_customer, name='create_customer'),
     path('service/edit_employee', views.edit_employee, name='edit_employee'),
+    path('service/edit_employee_object/<int:id>', views.edit_employee_object, name='edit_employee_object'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Web_Computer_Service/account/views.py
+++ Web_Computer_Service/account/views.py
@@ -1,20 +1,21 @@
-from django.forms.forms import Form
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView, FormView
 from django.views.generic.base import TemplateView
 from django.utils.crypto import get_random_string
+from django.contrib import messages
 from .forms import EmployeeCreationForm, CustomerCreationForm
 from .models import User
 
 def main(request):
     return render(request, 'index.html')
 
-class MainPage(LoginView):
+class LoginPage(LoginView):
     template_name = "account/index.html"
 
 class ServiceMainPage(TemplateView):
     template_name = 'service.html'
+
 
 def create_employee(request):
     if request.method == "POST":
@@ -34,6 +35,20 @@ def create_employee(request):
     else:
         form = EmployeeCreationForm()
     return render(request, 'service_functions/add_employee.html', {'form': form})
+
+def edit_employee_object(request, id):
+    employee = get_object_or_404(User, pk=id)
+    employee_password = employee.password
+    form = EmployeeCreationForm(request.POST or None, instance = employee)
+    
+    if form.is_valid():
+        employee_temp = form.save(commit=False)
+        employee_temp.set_password(employee_password)
+        employee_temp.save()
+        messages.success(request, 'Konto pomyślnie zaktualizowane')
+        return redirect('Web_Computer_Service:service')
+
+    return render(request, 'service_functions/edit_employee_object_instance.html', {'form': form})
 
 def create_customer(request):
     if request.method == "POST":
